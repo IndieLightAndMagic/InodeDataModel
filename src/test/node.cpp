6c09ba2784186ae7329cc6ecb3ae76b9c8cc245b
Julian Andres Guarin Reyes <julian.guarin@globant.com>
2017-12-01 18:02:42 -0500

    INode testing

--- src/test/node.cpp
+++ src/test/node.cpp
@@ -1,8 +1,43 @@
 #include "Device.h"
 
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+class test{
+
+	int random_variable;
+public:
+	int routers,customers,switches;
+	test(){
+		std::srand(std::time(0));
+		random_variable = rand();
+
+		routers = (random_variable & 0xf) + 1;
+		random_variable >>= 4;
+		switches = (random_variable & 0x2f) + 1;
+		random_variable >>= 6;
+		customers = (random_variable & 0xff) + 1;
+
+		cout << "routers : " << routers << "\n";
+		cout << "switches : " << switches << "\n";
+		cout << "customers : " << customers << "\n";
+	
+
+	}
+
+};
+
+
 int main(void)
 {
 
+	test a;
+
+	
+	
 	std::shared_ptr<Inode<float>> i = std::make_shared<Device>();
 	std::shared_ptr<Device> j = std::make_shared<Device>();	
 	i->connectSinkNode(j);
