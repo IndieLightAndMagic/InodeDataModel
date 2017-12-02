ca5c194c42b59632be3796b289fc653563e73c0c
Julian Andres Guarin Reyes <julian.guarin@globant.com>
2017-12-01 19:03:47 -0500

    INode WIP

--- src/Device.h
+++ src/Device.h
@@ -8,17 +8,22 @@ using namespace std;
 class Device : public Inode<float> {
 
 protected:
-
 	string name;
 public:
-	void connectSinkNode(shared_ptr<Inode<float>> p) override{ cout << "SINK\n";}
-	void connectSourceNode(shared_ptr<Inode<float>> p) override{ cout << "SOURCE\n";}
-
-	~Device() override{ cout << "KILLING\n"; }
+	void connectSinkNode(shared_ptr<Inode<float>> p) override{ 
+		auto t = tuple<float,float,shared_ptr<Inode<float>>>(0.0f, 0.0f, p);
+		sinks.push_back(t); 
+		cout << "SINK\n";
+	}
+	void connectSourceNode(shared_ptr<Inode<float>> p) override{ 
+		auto t = tuple<float,float,shared_ptr<Inode<float>>>(0.0f, 0.0f, p);
+		sources.push_back(t); 
+		cout << "SOURCE\n";
+	}
+	~Device() override{ }
 
 
 };
-
 
 
 #endif /* __DEVICE_H__ */
--- src/test/node.cpp
+++ src/test/node.cpp
@@ -34,13 +34,41 @@ public:
 int main(void)
 {
 
+	auto produceDevices = [](int nDevices)
+	{
+		std::vector<std::shared_ptr<Inode<float>>>devices;
+		while (nDevices--)
+		{
+			devices.push_back(std::make_shared<Device>());
+		}
+		return devices;
+	};
+
+	auto relateParentsAndSons = [](auto parents, auto sons, std::string message)
+	{	
+		auto nParents = parents.size();
+		cout << message << " >\n";
+		auto selectSonByIndex = 0;
+		for(auto son:sons)
+		{
+			auto selectParentByIndex = rand()%nParents;
+			son -> connectSourceNode(parents[selectParentByIndex]);
+			parents[selectParentByIndex] -> connectSinkNode(son);
+			cout << "parent: " << selectParentByIndex << " -> son: " << selectSonByIndex++ << "\n";  
+		}
+
+	};
+
 	test a;
 
-	
-	
-	std::shared_ptr<Inode<float>> i = std::make_shared<Device>();
-	std::shared_ptr<Device> j = std::make_shared<Device>();	
-	i->connectSinkNode(j);
+
+	auto routers = produceDevices(a.routers);
+	auto switches = produceDevices(a.switches);
+	auto customers = produceDevices(a.customers);
+
+	relateParentsAndSons(routers,switches,"routers parenting switches!");
+	relateParentsAndSons(switches,customers,"switches parenting customers!");
+
 	return 0;
 
 }
